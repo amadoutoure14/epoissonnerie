65768b1a841df5c2be801cfbe4fdd0f113a89337
Amadou Touré <131898454+amadoutoure14@users.noreply.github.com>
2024-03-14 08:01:44 +0000

    LA CONFIGURATION DE LA CONNEXION

--- src/main/java/com/source/epoissonnerie/controller/UtilisateurController.java
+++ src/main/java/com/source/epoissonnerie/controller/UtilisateurController.java
@@ -5,14 +5,16 @@ import com.source.epoissonnerie.entites.Utilisateur;
 import com.source.epoissonnerie.services.UtilisateurService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
@@ -34,7 +36,7 @@ public class UtilisateurController {
     @PostMapping(value = "/connexion")
     public Map<String,String> connexion(@RequestBody AuthentificationDTO authentificationDTO){
         final Authentication authenticate = authentificationManager.authenticate(new UsernamePasswordAuthenticationToken(authentificationDTO.username(), authentificationDTO.password()));
-        log.info("resultatr {}", authenticate.isAuthenticated());
+        log.info("resultat {}", authenticate.isAuthenticated());
         return null;
     }
 }
